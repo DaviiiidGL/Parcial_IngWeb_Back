d60e5b2c5b1edbd520d69cfe220081754f566680
DaviiiidGL <david.lema2509@gmail.com!git config --global user.email>
2026-03-19 08:22:44 -0500

    Agregar archivos de proyecto.

---
+++ Models/Preguntas.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Parcial_IngWeb.Models
+{
+    public class Preguntas
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid IdPregunta { get; set; }
+
+        [MinLength(5)]
+        [MaxLength(100)]
+        public string Enunciado { get; set; }
+        [MinLength(2)]
+        [MaxLength(30)]
+        public string Categoria { get; set; }
+
+        public bool isAnswered { get; set; }
+    }
+}
---
+++ Models/Respuestas.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Parcial_IngWeb.Models
+{
+    public class Respuestas
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid IdRespuesta { get; set; }
+        [Required]
+        public Guid IdPregunta { get; set; }
+
+        [ForeignKey("IdPregunta")]
+        public Preguntas? Pregunta { get; set; }
+
+        [MinLength(2)]
+        [MaxLength(500)]
+        public string Contenido { get; set; }
+        public DateTime FechaCreacion { get; set; } = DateTime.UtcNow;
+    }
+}
